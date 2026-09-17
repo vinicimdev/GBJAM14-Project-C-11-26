cf5c7ee28e67054c02918ab6727397bcaf28f113
Vinicius Januzzi <contatovinij@gmail.com>
2026-09-16 22:18:51 -0700

    ADDED health bar controller script

---
+++ Assets/Scripts/Player/HealthBarController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Updates a Unity UI Slider to display the CharacterHealth's current and max health.
+/// </summary>
+public class HealthBarController : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField, Tooltip("")]
+    private CharacterHealth health;
+    [SerializeField, Tooltip("")]
+    private Slider slider;
+
+    private void OnEnable()
+    {
+        slider.minValue = 0f;
+        slider.maxValue = health.MaxHealth;
+        slider.value = health.CurrentHealth;
+
+        health.OnHealthChanged += UpdateSlider;
+    }
+
+    private void OnDisable()
+    {
+        health.OnHealthChanged -= UpdateSlider;
+    }
+
+    private void UpdateSlider(int currentHealth)
+    {
+        slider.value = currentHealth;
+    }
+}
