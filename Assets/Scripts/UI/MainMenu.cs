2b428c36f58c788ed678261c5061132cf24c400e
Jhonnatan Barbosa <jhocbarbosa@gmail.com>
2026-09-18 12:23:04 -0300

    MODIFIED menu to make selections with the keys too

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,14 +1,45 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string cutsceneScene = "Cutscene";
     [SerializeField] GameObject creditsWindow;
+    [SerializeField] GameObject playButton;
+    [SerializeField] GameObject closeCreditsButton;
+
+    GameObject selected;
 
     void Awake() => creditsWindow.SetActive(false);
 
+    void Start() => Select(playButton);
+
+    void Update()
+    {
+        // Navigate goes nowhere while nothing is selected, and the pointer empties it.
+        GameObject current = EventSystem.current.currentSelectedGameObject;
+        if (current != null) selected = current;
+        else EventSystem.current.SetSelectedGameObject(selected);
+    }
+
     public void Play() => SceneManager.LoadScene(cutsceneScene);
-    public void OpenCredits() => creditsWindow.SetActive(true);
-    public void CloseCredits() => creditsWindow.SetActive(false);
+
+    public void OpenCredits()
+    {
+        creditsWindow.SetActive(true);
+        Select(closeCreditsButton);
+    }
+
+    public void CloseCredits()
+    {
+        creditsWindow.SetActive(false);
+        Select(playButton);
+    }
+
+    void Select(GameObject button)
+    {
+        selected = button;
+        EventSystem.current.SetSelectedGameObject(button);
+    }
 }
