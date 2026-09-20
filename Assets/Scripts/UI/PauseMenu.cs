8868e28ada56460cf15c0354140365ae58980264
Jhonnatan Barbosa <jhocbarbosa@gmail.com>
2026-09-20 12:52:23 -0300

    MODIFIED pause window to work on keyboard

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,16 +1,33 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     [SerializeField] InputActionReference pause;
+    [SerializeField] string menuScene = "Menu";
     [SerializeField] GameObject pauseWindow;
-    [SerializeField] GameObject firstSelected;
+    [SerializeField] GameObject resumeButton;
+    [SerializeField] GameObject creditsWindow;
+    [SerializeField] GameObject closeCreditsButton;
+    [SerializeField] GameObject howToWindow;
+    [SerializeField] GameObject closeHowToButton;
+
+    [SerializeField] AudioSource sfx;
+    [SerializeField] AudioClip moveClip;
+    [SerializeField] AudioClip clickClip;
 
     bool isPaused;
+    GameObject selected;
 
-    void Awake() => pauseWindow.SetActive(false);
+    void Awake()
+    {
+        pauseWindow.SetActive(false);
+        creditsWindow.SetActive(false);
+        howToWindow.SetActive(false);
+    }
 
     void OnEnable()
     {
@@ -28,11 +45,78 @@ public class PauseMenu : MonoBehaviour
 
     void OnPause(InputAction.CallbackContext ctx) => Toggle();
 
+    void Update()
+    {
+        if (!isPaused) return;
+
+        GameObject current = EventSystem.current.currentSelectedGameObject;
+        if (current != null)
+        {
+            if (current != selected) Blip(moveClip);
+            selected = current;
+        }
+        else EventSystem.current.SetSelectedGameObject(selected);
+    }
+
     public void Toggle()
     {
         isPaused = !isPaused;
-        pauseWindow.SetActive(isPaused);
+        Blip(clickClip);
         Time.timeScale = isPaused ? 0f : 1f;
-        EventSystem.current.SetSelectedGameObject(isPaused ? firstSelected : null);
+        pauseWindow.SetActive(isPaused);
+        creditsWindow.SetActive(false);
+        howToWindow.SetActive(false);
+
+        if (isPaused) Select(resumeButton);
+        else EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    public void ToMenu() => StartCoroutine(ToMenuRoutine());
+
+    public void OpenCredits()
+    {
+        Blip(clickClip);
+        creditsWindow.SetActive(true);
+        Select(closeCreditsButton);
+    }
+
+    public void CloseCredits()
+    {
+        Blip(clickClip);
+        creditsWindow.SetActive(false);
+        Select(resumeButton);
+    }
+
+    public void OpenHowTo()
+    {
+        Blip(clickClip);
+        howToWindow.SetActive(true);
+        Select(closeHowToButton);
+    }
+
+    public void CloseHowTo()
+    {
+        Blip(clickClip);
+        howToWindow.SetActive(false);
+        Select(resumeButton);
+    }
+
+    IEnumerator ToMenuRoutine()
+    {
+        Blip(clickClip);
+        if (clickClip != null) yield return new WaitForSecondsRealtime(clickClip.length);
+
+        SceneManager.LoadScene(menuScene);
+    }
+
+    void Blip(AudioClip clip)
+    {
+        if (sfx != null && clip != null) sfx.PlayOneShot(clip);
+    }
+
+    void Select(GameObject button)
+    {
+        selected = button;
+        EventSystem.current.SetSelectedGameObject(button);
     }
 }
